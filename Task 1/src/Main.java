3def57dbd43cf02daa1a34d64d10dd0742d555db
dem0N4k <demON4k1993@gmail.com>
2023-08-15 23:53:03 +0300

    finish

--- Task 1/src/Main.java
+++ Task 1/src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
 
+        Phone Poco = new Phone("375295528575", "Poco", 213);
+        Phone iPhone = new Phone("375445507983", "iPhone 14", 185);
+        Phone noname = new Phone();
+        Phone.manufacturerCountry = "China";
+
+        System.out.println("Модель телефона " + Poco.model + " весом " + Poco.wheight
+                + " с номером телефона " + Poco.number + " создан в " + Poco.manufacturerCountry);
+        System.out.println("Модель телефона " + iPhone.model + " весом " + iPhone.wheight
+                + " с номером телефона " + iPhone.number + " создан в ");
+        System.out.println("У Евгения телефон " + noname.model + " а его номер телефона " +
+                noname.number + " создан в " + noname.manufacturerCountry);
+        System.out.println("\n");
+
+        Poco.receiveCall("Владимир");
+        iPhone.receiveCall("Владислав", "29948618");
+        noname.receiveCall("Евгений");
+
+        Phone.manufacturerCountry = "China";
     }
 }
--- Task 1/src/Phone.java
+++ Task 1/src/Phone.java
@@ -1,2 +1,29 @@
 public class Phone {
+
+    static String manufacturerCountry;
+    String number;
+    String model;
+    double wheight;
+
+    public Phone() {
+    }
+
+    public Phone(String nomber, String model, double wheight) {
+        this.number = nomber;
+        this.model = model;
+        this.wheight = wheight;
+
+    }
+
+    public void receiveCall(String callerName) {
+        receiveCall(callerName, "");
+    }
+
+    public void receiveCall(String callerName, String number) {
+        System.out.println("Звонит абонент по имени " + callerName + (number == "" ? "" : "номер " + number));
+    }
+
+    public void Phone() {
+        System.out.println("Модель телефона " + model + " произведена в стране " + manufacturerCountry);
+    }
 }
